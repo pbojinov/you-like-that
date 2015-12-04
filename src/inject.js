23ee0f22b31a38a796b2501382565b6a1a631e7b
Petar Bojinov <petarbojinov@gmail.com>
2015-12-03 23:56:14 -0800

    got it working with instagram

--- src/inject.js
+++ src/inject.js
@@ -1,5 +1,17 @@
 console.log("Hello. This message was sent from scripts/inject.js");
 
-var myAudio = new Audio();        // create the audio object
-myAudio.src = "https://a.clyp.it/4wdkdhn5.mp3"; // assign the audio file to it
-myAudio.play();   
+function playSound() {
+	var myAudio = new Audio();        // create the audio object
+	myAudio.src = "https://a.clyp.it/4wdkdhn5.mp3"; // assign the audio file to it
+	myAudio.play();   
+}
+
+// instagram
+$('.coreSpriteHeartOpen').on('click', function() {
+	playSound();
+});
+
+// facebook - not working
+$('.UFILikeLink').on('click', function() {
+	playSound();
+});
